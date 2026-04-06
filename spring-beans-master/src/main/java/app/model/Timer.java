57497eacf12acc12de673f732c26937f8168f68a
Piter <Piter.canada13@gmail.com>
2026-04-06 14:37:06 +0300

    213

---
+++ spring-beans-master/src/main/java/app/model/Timer.java
@@ -0,0 +1,24 @@
+//package app.model;
+//
+//public class Timer {
+//
+//    private Long nanoTime = System.nanoTime();
+//
+//    public Long getTime() {
+//        return nanoTime;
+//    }
+//}
+
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
